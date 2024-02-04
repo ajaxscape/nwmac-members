19a4819d0d69d4784fb83bbb2d9204ef49b81080
ben <ben@ajaxscape.com>
2024-02-04 14:53:09 +0000

    Second commit

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
-const govukFrontend = require('govuk-frontend');
+const path = require("path");
+const sass = require('sass');
+const { writeFileSync } = require('fs-extra');
+const {ensureDir} = require("fs-extra");
 
 const app = express();
 const port = 3000;
 
+// Compile SASS
+const result = sass.compile(path.join(__dirname, 'src/assets/sass/index.scss'), {
+    quietDeps: true,
+    sourceMap: true,
+    sourceMapIncludeSources: true,
+    style: 'expanded'
+})
+
+ensureDir(path.join(__dirname, 'public/css'), () => {
+    writeFileSync(path.join(__dirname, 'public/css/index.css'), result.css)
+})
+
 // Set up Nunjucks templating engine
-nunjucks.configure('views', {
+nunjucks.configure(['node_modules/govuk-frontend/dist', 'src/views'], {
     autoescape: true,
     express: app,
 });
 
+// Serve static assets from public
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
 // Serve static assets from govuk-frontend
-app.use('/govuk-frontend', express.static(govukFrontend.assetPath));
+app.use('/assets', express.static(path.join(__dirname, 'node_modules/govuk-frontend/dist/govuk/assets')));
 
 // Set the view engine to Nunjucks
 app.set('view engine', 'njk');
