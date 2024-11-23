fb78468591ba0545eb669f4cef866a49cd1d4b79
Ben Surgison <ben@ajaxscape.com>
2024-11-23 17:13:01 +0000

    Update redirect URL in email entry controller
    Changed the redirect destination from '/details/restore-data' to '/auth/restore-data' in the enter-email controller. This ensures that the email restoration process is directed to the correct authentication route.

--- src/app/auth/enter-email/enter-email.controller.js
+++ src/app/auth/enter-email/enter-email.controller.js
@@ -5,7 +5,7 @@ export const viewEnterEmail = (req, res) => {
   const email = req.signedCookies.email
   if (email) {
     req.session.email = email
-    res.redirect('/details/restore-data')
+    res.redirect('/auth/restore-data')
   } else {
     res.render('pages/auth/enter-email', {
       locals: res.locals,
