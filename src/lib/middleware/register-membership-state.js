869d8ba178b1df53e1b13466817b15b6d7085d3b
Ben Surgison <ben@ajaxscape.com>
2024-11-28 10:37:09 +0000

    Fix edit state bug

--- src/lib/middleware/register-membership-state.js
+++ src/lib/middleware/register-membership-state.js
@@ -1,4 +1,4 @@
 export default (req, res, next) => {
-  res.locals.edit = req.url.split('/').some((tag) => tag === 'edit')
+  res.locals.edit = req.originalUrl.split('/').includes('edit')
   next()
 }
