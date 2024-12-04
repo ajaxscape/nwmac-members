1a3bfcb6563e22ec32782aa7f5d32082d32d5a30
Ben Surgison <ben@ajaxscape.com>
2024-12-04 11:08:21 +0000

    do not allow caa through club flag to be changed when paid

--- src/app/details/caa-registration/caa-registration.controller.js
+++ src/app/details/caa-registration/caa-registration.controller.js
@@ -1,20 +1,29 @@
 import { validationResult } from 'express-validator'
 import { storeData } from '#utils/store-session-data.js'
 import { redirectUrl } from '#utils/redirect-url.js'
+import memberSubscriptionStatuses from '#utils/member-subscription-statuses.js'
 
-export const viewEnterCaaOperatorRegistration = (req, res) => {
+export const viewEnterCaaOperatorRegistration = async (req, res) => {
+  const { subscriptionPaymentMade } = await memberSubscriptionStatuses(
+    req.session.memberId
+  )
   res.render('pages/details/caa-registration', {
     operatorIdRequired: req.session.ageGroup === 'senior',
-    locals: res.locals
+    locals: res.locals,
+    subscriptionPaymentMade
   })
 }
 
-export const postEnterCaaOperatorRegistration = (req, res) => {
+export const postEnterCaaOperatorRegistration = async (req, res) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
+    const { subscriptionPaymentMade } = await memberSubscriptionStatuses(
+      req.session.memberId
+    )
     return res.render('pages/details/caa-registration', {
       operatorIdRequired: req.session.ageGroup === 'senior',
       locals: res.locals,
+      subscriptionPaymentMade,
       errors: errors.array()
     })
   }
