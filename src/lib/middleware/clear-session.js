fb8aa020a56e0c1527208dccc2d769b4893fbf04
Ben Surgison <ben@ajaxscape.com>
2024-12-08 13:23:17 +0000

    Allow next url to work correctly

--- src/lib/middleware/clear-session.js
+++ src/lib/middleware/clear-session.js
@@ -1,7 +1,9 @@
 export default (req, res, next) => {
   // Clear session
   for (const sessionKey of Object.keys(req.session)) {
-    delete req.session[sessionKey]
+    if (sessionKey !== 'nextUrl') {
+      delete req.session[sessionKey]
+    }
   }
   next()
 }
