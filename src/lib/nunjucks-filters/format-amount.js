1b1f79e585c9e4ca569ac1c3adc7e36d27309dbc
ben <ben@ajaxscape.com>
2024-12-10 18:08:14 +0000

    Add validation to formatAmount and corresponding tests

--- src/lib/nunjucks-filters/format-amount.js
+++ src/lib/nunjucks-filters/format-amount.js
@@ -1,3 +1,6 @@
 export default function formatAmount(amountInPence, prefix = '&pound;') {
+  if (isNaN(amountInPence) || amountInPence === null) {
+    throw new TypeError('Amount must be a number')
+  }
   return `${prefix}${(amountInPence / 100).toFixed(2)}`
 }
